88269090bb48d92b13a1871370f819c1e3456ed4
aleksandr.permyakov <aleksandr.permyakov@npo-at.com>
2021-12-20 22:07:27 +0500

    added event model and migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Post, Project
+from .models import Post, Project, Event
 
 
 class ProjectAdmin(admin.ModelAdmin):
     fields = ('title', 'description', 'created_at', 'thumbnail')
 
 
+class EventAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'created_at', 'thumbnail', 'project')
+
+
 admin.site.register(Post)
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Event, EventAdmin)
