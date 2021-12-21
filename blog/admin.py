2a67fb351f02ff80b46f4dbd489e94e5ad9c761e
aleksandr.permyakov <aleksandr.permyakov@npo-at.com>
2021-12-21 23:09:28 +0500

    added events details and dir models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Project, Event
+from .models import Project, Event, Directory
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -10,6 +10,10 @@ class EventAdmin(admin.ModelAdmin):
     fields = ('title', 'description', 'created_at', 'thumbnail', 'project')
 
 
-admin.site.register(Post)
+class DirectoryAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'created_at', 'thumbnail', 'event', 'parent_dir')
+
+
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.register(Directory, DirectoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,10 @@ def file_path_dir(instance, filename):
     return 'uploads/project/{0}/{1}/{2}'.format(instance.project.id, instance.id, filename)
 
 
+def dir_thumb_path_dir(instance, filename):
+    return 'uploads/project/{0}/{1}/{2}'.format(instance.event.project.id, instance.id, filename)
+
+
 class Post(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=50)
@@ -31,7 +35,20 @@ class Event(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField()
     thumbnail = models.FileField(upload_to=file_path_dir)
-    project = models.ForeignKey(Project, on_delete=models.PROTECT,)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
+
+
+class Directory(models.Model):
+    objects = models.Manager()
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField()
+    thumbnail = models.FileField(upload_to=dir_thumb_path_dir)
+    event = models.ForeignKey(Event, on_delete=models.PROTECT, related_name='directories')
+    parent_dir = models.ForeignKey('Directory', on_delete=models.PROTECT, null=True, blank=True, related_name='child_dirs')
+
+    def __str__(self):
+        return self.event.title + ': ' + self.title
