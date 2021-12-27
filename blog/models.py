6952493bc0702854c03d8177155de98c07a5ddda
aleksandr.permyakov <aleksandr.permyakov@npo-at.com>
2021-12-27 16:49:56 +0500

    squashed migrations into initial one, fixed None directory bug

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,7 @@ from django.db import models
 
 
 def file_path_dir(instance, filename):
-    return 'uploads/project/{0}/{1}/{2}'.format(instance.project.id, instance.id, filename)
-
-
-def project_path_dir(instance, filename):
-    return 'uploads/project/{0}/{1}/{2}'.format(instance.parent_dir.id, instance.id, filename)
-
-
-def dir_thumb_path_dir(instance, filename):
-    return 'uploads/project/{0}/{1}/{2}'.format(instance.event.project.id, instance.id, filename)
+    return 'uploads/project/{0}/{1}'.format(instance.parent_dir.id, filename)
 
 
 class Project(models.Model):
@@ -20,7 +12,7 @@ class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_at = models.DateTimeField()
-    thumbnail = models.FileField(upload_to='uploads/project/')
+    thumbnail = models.FileField(upload_to='uploads/project/preview/')
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self):
@@ -32,7 +24,7 @@ class Event(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_at = models.DateTimeField()
-    thumbnail = models.FileField(upload_to=file_path_dir)
+    thumbnail = models.FileField(upload_to='uploads/project/preview/')
     project = models.ForeignKey(Project, on_delete=models.PROTECT)
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
 
@@ -44,7 +36,6 @@ class Directory(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField()
-    thumbnail = models.FileField(upload_to=dir_thumb_path_dir)
     event = models.ForeignKey(Event, on_delete=models.PROTECT, related_name='directories')
     parent_dir = models.ForeignKey('Directory', on_delete=models.PROTECT, null=True, blank=True, related_name='child_dirs')
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -57,7 +48,7 @@ class File(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField()
-    file = models.FileField(upload_to=project_path_dir)
+    file = models.FileField(upload_to=file_path_dir)
     size = models.BigIntegerField()
     parent_dir = models.ForeignKey(Directory, on_delete=models.PROTECT, related_name='files')
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
