84a8cb653f0a438148d1cc44f03c4a6f7eb5cddc
aleksandr.permyakov <aleksandr.permyakov@npo-at.com>
2023-06-14 17:44:32 +0500

    updated filters form

--- blog/filters.py
+++ blog/filters.py
@@ -1,8 +1,9 @@
 import django_filters
 from .models import Project
+from django.utils.translation import gettext as _
 
 class ProjectFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains', label='Filter by title')
+    title = django_filters.CharFilter(lookup_expr='icontains', label=_('Filter by title'))
 
     class Meta:
         model = Project
